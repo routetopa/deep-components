7a60f3f350e2e0d24fbc6b4299f5a1b0454b3cc1
andrpet <andrpet@gmail.com>
2016-01-08 17:21:42 +0100

    l20n bug fix

---
+++ locales/lnHashMap.js
@@ -0,0 +1,52 @@
+var ln = [];
+
+ln["slide1Title_it"] = "SELEZIONA IL DATASET";
+ln["slide1Subtitle_it"] = "Ricerca o copia e incolla la url del dataset.";
+ln["slide2Title_it"] = "SELEZIONA I DATI";
+ln["slide2Subtitle_it"] = "Seleziona i campi dalla struttura ad albero. Nella tabella saranno mostrati i valori dei campi selezionati.";
+ln["slide3Title_it"] = "SELEZIONA LA VISUALIZZAZIONE";
+ln["slide3Subtitle_it"] = "Seleziona una visualizzazione, inserisci dati di input ed etichette (opzionale).";
+
+ln["listView_it"] = "ELENCO";
+ln["treeMapView_it"] = "GRAFICO AD ALBERO";
+ln["mostPopular_it"] = "I PIÙ VISTI";
+ln["availableDatasets_it"] = "Dataset disponibili";
+ln["suggestedDatasets_it"] = "Dataset suggeriti";
+ln["selectedUrl_it"] = "Url selezionata";
+
+ln["inputs_it"] = "DATI DI INPUT";
+ln["layouts_it"] = "ETICHETTE / OPZIONI";
+ln["baseInfo_it"] = "INFORMAZIONI DI BASE";
+ln["comment_it"] = "COMMENTO";
+ln["commentLabel_it"] = "commento (massimo 100 caratteri)";
+ln["dataletPreview_it"] = "ANTEPRIMA";
+ln["addDatalet_it"] = "AGGIUNGI";
+ln["modifyDatalet_it"] = "MODIFICA";
+
+ln["cellDescription_it"] = "Cella della tabella.";
+ln["levelDescription_it"] = "Livello utilizzato per il raggruppamento dei valori corrispondenti.";
+
+ln["categoriesDescription_it"] = "Categorie del grafico. I valori saranno inseriti sull’asse orizzontale. Inserire un valore per ciascuna serie.";
+ln["seriesDescription_it"] = "Serie del grafico. I valori saranno inseriti sull’asse verticale.";
+
+ln["XAxisLabelDescription_it"] = "Etichetta asse X.";
+ln["YAxisLabelDescription_it"] = "Etichetta asse Y.";
+ln["suffixDescription_it"] = "Suffisso valore (e.g. unità di misura, dollari, euro,…).";
+
+ln["pieSeriesDescription_it"] = "Serie del grafico. I valori saranno inseriti sulla porzione corrispondente alla relativa serie.";
+ln["pieSizeDescription_it"] = "Dimensione di ciascuna serie per creare anelli concentrici";
+
+ln["bubbleCategoriesDescription_it"] = "Serie del grafico. Inserire un valore per ciascuna serie.";
+ln["bubbleYValuesDescription_it"] = "Valori y del grafico. I valori saranno inseriti sull’asse verticale";
+ln["bubbleZValuesDescription_it"] = "Valori z del grafico. I valori rappresentano la dimensione della bolla.";
+ln["bubbleSeriesDescription_it"] = "Serie del grafico. I valori visualizzano un grafico a bolle a serie multipla.";
+
+ln["scatterCategoriesDescription_it"] = "Categorie del grafico. Inserire un valore per ciascuna serie.";
+ln["scatterXValuesDescription_it"] = "Serie del grafico. I valori saranno inseriti sull’asse orizzontale.";
+ln["scatterYValuesDescription_it"] = "Serie del grafico. I valori saranno inseriti sull’asse verticale..";
+
+ln["heatmapCategoriesDescription_it"] = "Categorie del grafico. I valori saranno inseriti sull’asse orizzontale.";
+
+ln["LatitudeDescription_it"] = "Latitudine del punto da riportare sulla mappa.";
+ln["LongitudeDescription_it"] = "Longitudine del punto da riportare sulla mappa.";
+ln["BalloonContentDescription_it"] = "Descrizione del segnaposto o link all'immagine che visualizza il luogo specificato sulla mappa.";
