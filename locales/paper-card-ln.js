ed70e2e235ca261f7d72e237fbb441b4600be78c
andrpet <andrpet@gmail.com>
2016-01-22 12:24:00 +0100

    tooltip internationalization

---
+++ locales/paper-card-ln.js
@@ -0,0 +1,18 @@
+ln = [];
+
+ln["modify_it"] = "Modify card";
+ln["delete_it"] = "Delete card";
+ln["open_it"]   = "Open preview";
+
+ln["modify_en"] = "Modify card";
+ln["delete_en"] = "Delete card";
+ln["open_en"]   = "Open preview";
+
+ln["modify_fr"] = "Modify card";
+ln["delete_fr"] = "Delete card";
+ln["open_fr"]   = "Open preview";
+
+ln["modify_nl"] = "Modify card";
+ln["delete_nl"] = "Delete card";
+ln["open_nl"]   = "Open preview";
+
