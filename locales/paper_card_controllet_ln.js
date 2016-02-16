2813afd728d651a9d9d774fcd41c3da4399cb910
renato <rended83@gmail.com>
2016-02-16 16:28:03 +0100

    locales ln mod

---
+++ locales/paper_card_controllet_ln.js
@@ -0,0 +1,18 @@
+paper_ln = [];
+
+paper_ln["modify_it"] = "Modifica card";
+paper_ln["delete_it"] = "Elimina card";
+paper_ln["open_it"]   = "Apri anteprima";
+
+paper_ln["modify_en"] = "Modify card";
+paper_ln["delete_en"] = "Delete card";
+paper_ln["open_en"]   = "Open preview";
+
+paper_ln["modify_fr"] = "Modifier la card";
+paper_ln["delete_fr"] = "Supprimer la card";
+paper_ln["open_fr"]   = "Ouvrir aperçu";
+
+paper_ln["modify_nl"] = "Wijzigen card";
+paper_ln["delete_nl"] = "Wissen card";
+paper_ln["open_nl"]   = "Open preview";
+
