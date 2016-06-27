25201c03867bf5bcadd43f942691c0580871a3d6
renato <rended83@gmail.com>
2016-06-27 16:30:05 +0200

    spinner, geojsonPure

--- controllets/providers-utility-controllet/generic.js
+++ controllets/providers-utility-controllet/generic.js
@@ -1,9 +1,20 @@
 function generic_Provider () {}
 
 generic_Provider.prototype.selectData = function(data) {
-    return data;
+
+    if(data instanceof Array)
+        return data;
+    if(_isGEOJSON(data))
+        return [{"GEOJSON" : data}];
+    if(data instanceof Object)
+        return [{"JSON" : data}];
 };
 
 generic_Provider.prototype.addLimit = function(url) {
     return url;
+};
+
+function _isGEOJSON (data) {
+    var dt = new DataTypeConverter();
+    return (dt.inferDataSubTypeOfValue(data) && dt.inferDataSubTypeOfValue(data).name == DataTypeConverter.SUBTYPES.GEOJSON.name);
 };
