44cffc23ebd1089015306f1a88ffff10701a7154
andrpet <andrpet@gmail.com>
2016-09-23 12:28:31 +0200

    plugin update

---
+++ locales/base_datalet_ln.js
@@ -0,0 +1,25 @@
+base_datalet_ln = [];
+
+/*EN*/
+base_datalet_ln["export_HTML_en"]    = "Export to HTML";
+base_datalet_ln["export_PNG_en"]     = "Export to PNG";
+base_datalet_ln["export_RTF_en"]     = "Export to RTF";
+base_datalet_ln["import_myspace_en"] = "Import in My Space";
+
+/*IT*/
+base_datalet_ln["export_HTML_it"]    = "Esporta in HTML";
+base_datalet_ln["export_PNG_it"]     = "Esporta in PNG";
+base_datalet_ln["export_RTF_it"]     = "Esporta in RTF";
+base_datalet_ln["import_myspace_it"] = "Importa nello Spazio privato";
+
+/*FR*/
+base_datalet_ln["export_HTML_fr"]    = "Export to HTML";
+base_datalet_ln["export_PNG_fr"]     = "Export to PNG";
+base_datalet_ln["export_RTF_fr"]     = "Export to RTF";
+base_datalet_ln["import_myspace_fr"] = "Import in My Space";
+
+/*NL*/
+base_datalet_ln["export_HTML_nl"]    = "Export to HTML";
+base_datalet_ln["export_PNG_nl"]     = "Export to PNG";
+base_datalet_ln["export_RTF_nl"]     = "Export to RTF";
+base_datalet_ln["import_myspace_nl"] = "Import in My Space";
