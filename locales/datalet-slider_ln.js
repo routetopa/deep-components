abb3c5f2d27ea4dd5a8f06bcdf34986241d40b8c
renato <rended83@gmail.com>
2016-10-21 15:23:31 +0200

    new datalet slider!

---
+++ locales/datalet-slider_ln.js
@@ -0,0 +1,23 @@
+dataletslider_ln = [];
+
+dataletslider_ln["ln"] = "en";
+
+/*EN*/
+dataletslider_ln["noDatalet_en"] = "There are no datalets to show";
+dataletslider_ln["back_en"] = "BACK";
+dataletslider_ln["forward_en"] = "FORWARD";
+
+/*IT*/
+dataletslider_ln["noDatalet_it"] = "There are no datalets to show";
+dataletslider_ln["back_it"] = "BACK";
+dataletslider_ln["forward_it"] = "FORWARD";
+
+/*FR*/
+dataletslider_ln["noDatalet_fr"] = "There are no datalets to show";
+dataletslider_ln["back_fr"] = "BACK";
+dataletslider_ln["forward_fr"] = "FORWARD";
+
+/*NL*/
+dataletslider_ln["noDatalet_nl"] = "There are no datalets to show";
+dataletslider_ln["back_nl"] = "BACK";
+dataletslider_ln["forward_nl"] = "FORWARD";
