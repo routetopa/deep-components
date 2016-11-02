290e31196eb3bdfbb74f232befa2dfad80073196
rended <renato.dedonato@gmail.com>
2016-11-02 12:02:28 +0100

    new info controllet

--- locales/info_list_ln.js
+++ locales/info_list_ln.js
@@ -3,10 +3,61 @@ info_ln = [];
 info_ln["ln"] = "en";
 
 /*EN*/
-//info_ln["info_en"] = "INFO";
+info_ln["info_title_en"] = "INFO";
+
+info_ln["name_en"] = "Name";
+info_ln["subject_en"] = "Subject";
+info_ln["description_en"] = "Description";
+info_ln["goal_en"] = "Goal";
+info_ln["invitation_text_en"] = "Invitation Text";
+info_ln["period_en"] = "Period";
+info_ln["members_en"] = "Members";
+
+info_ln["joined_en"] = "JOINED";
+info_ln["pending_en"] = "PENDING";
+info_ln["owner_en"] = "OWNER";
 
 /*IT*/
+info_ln["info_title_it"] = "INFO";
+
+info_ln["name_it"] = "Nome";
+info_ln["subject_it"] = "Soggetto";
+info_ln["description_it"] = "Descrizione";
+info_ln["goal_it"] = "Gol";
+info_ln["invitation_text_it"] = "Testo di invito";
+info_ln["period_it"] = "Periodo";
+info_ln["members_it"] = "Membri";
+
+info_ln["joined_it"] = "JOINED";
+info_ln["pending_it"] = "PENDING";
+info_ln["owner_it"] = "OWNER";
 
 /*FR*/
+info_ln["info_title_fr"] = "INFO";
+
+info_ln["name_fr"] = "Name";
+info_ln["subject_fr"] = "Subject";
+info_ln["description_fr"] = "Description";
+info_ln["goal_fr"] = "Goal";
+info_ln["invitation_text_fr"] = "Invitation Text";
+info_ln["period_fr"] = "Period";
+info_ln["members_fr"] = "Members";
+
+info_ln["joined_fr"] = "JOINED";
+info_ln["pending_fr"] = "PENDING";
+info_ln["owner_fr"] = "OWNER";
 
 /*NL*/
+info_ln["info_title_nl"] = "INFO";
+
+info_ln["name_nl"] = "Name";
+info_ln["subject_nl"] = "Subject";
+info_ln["description_nl"] = "Description";
+info_ln["goal_nl"] = "Goal";
+info_ln["invitation_text_nl"] = "Invitation Text";
+info_ln["period_nl"] = "Period";
+info_ln["members_nl"] = "Members";
+
+info_ln["joined_nl"] = "JOINED";
+info_ln["pending_nl"] = "PENDING";
+info_ln["owner_nl"] = "OWNER";
