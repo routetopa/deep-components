a7cd06ea4eafcbb7ae1a28acfe12e9e6dff5c0de
rended <rended83@gmail.com>
2017-07-18 12:00:12 +0200

    limit datalet title&desc, OData provider utility

--- controllets/providers-utility-controllet/OData.js
+++ controllets/providers-utility-controllet/OData.js
@@ -1,69 +1,10 @@
-function ckan_Provider () { //--> dataUrl inutile
-    //this.dataUrl = dataUrl;
-    //this.path = "result->records";
-    //this.arrayPath = ["result", "records", "*"];
+function OData_Provider () {
 }
 
-ckan_Provider.prototype.selectData = function(data) {
-    return data.result.records;
+OData_Provider.prototype.selectData = function(data) {
+    return data.value;
 };
 
-ckan_Provider.prototype.addLimit = function(url) {
-    if(url.indexOf("&limit=") > -1)
-        return url;
-    return url + "&limit=99999";
+OData_Provider.prototype.addLimit = function(url) {
+    return url;
 };
-
-ckan_Provider.prototype.getHTMLFormattedMetadata = function(dataset, resourceIndex) {
-    dataset = dataset.result;
-
-    var html = '';
-
-    html += '<b>Resource Name:</b> <b style="color: #2196F3;">' + dataset.resources[resourceIndex].name + '</b><br>';
-    html += '<b>Resource Description:</b> ' + dataset.resources[resourceIndex].description + '</b><div style="height: 1px; background: #2196F3; margin: 12px 0;"></div>';
-
-    html += '<b>Organization:</b> ' + ((dataset.organization) ? dataset.organization.name : "") + '<br>';
-    html += '<b>Dataset Name:</b> ' + dataset.name + '<br>';
-    html += '<b>Dataset Title:</b> ' + dataset.title + '<div style="height: 1px; background: #2196F3; margin: 12px 0;"></div>';
-    // html += '<b>Resource Name:</b> <b style="color: #F44336;">' + dataset.resources[resourceIndex].name + '</b><div style="height: 1px; background: #F44336; margin: 12px 0;"></div>';
-    // html += '<b>Resource Description:</b> ' + dataset.resources[resourceIndex].description + '</b><div style="height: 1px; background: #F44336; margin: 12px 0;"></div>';
-
-    filters = ["id", "creator_user_id", "license_id", "owner_org", "revision_id", "name", "title", "notes", "num_resources", "num_tags", "state", "type"];
-
-    var orderedKeys = Object.keys(dataset).sort()
-
-    for(var i in orderedKeys) {
-        var key = orderedKeys[i];
-        var value = dataset[key];
-        if (value != null && value != undefined && String(value).trim() != "" && typeof(value) != 'object' && $.inArray(key, filters) == -1)
-            html += '<b>' + key + ':</b> ' + value + '<br>';
-    }
-
-    return html;
-};
-
-ckan_Provider.prototype.getDatasetUrl = function(providerUrl, datasetId) {
-    return providerUrl + '/api/3/action/package_show?id=' + datasetId;
-};
-
-ckan_Provider.prototype.getResourceUrl = function(providerUrl, resourceId) {
-    return providerUrl + '/api/3/action/datastore_search?resource_id=' + resourceId;
-};
-
-//    var fields = [];
-//    for (var key in data.result.records[0])
-//        fields.push(key);
-//    return fields;
-//};
-
-//ckan_Provider.prototype.requestData = function() {
-//    var that = this;
-//
-//    $.ajax({
-//        url: this.dataUrl,
-//        dataType: "json",
-//        success: function(data){
-//            that.data = data.result.records;
-//        }
-//    });
-//};
