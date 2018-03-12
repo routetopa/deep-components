74d2c841ba722faeeede8e5c1a7474d932ad338c
palmieri@dia.unisa.it <palmieri@dia.unisa.it>
2018-03-12 18:47:38 +0100

    Italian translation of maplet v.3.1

--- controllets/map-controllet/controllet_ln.js
+++ controllets/map-controllet/controllet_ln.js
@@ -1,5 +1,5 @@
-
 // MAP CONTROLLET
+/*EN*/
 
 ln["mc.map-controllet.slide1_title.en"] = "SELECT DATASET";
 ln["mc.map-controllet.slide1_subtitle.en"] = "Select a dataset from the list or copy and paste the url of dataset.";
@@ -9,10 +9,10 @@ ln["mc.map-controllet.slide2_subtitle.en"] = "Select the layers on the left. The
 ln["mc.map-controllet.addDatasetButton.en"] = "Add Dataset";
 ln["mc.map-controllet.addDataletButton.en"] = "Add";
 
-
 ln["mc.layers-list-controllet.title.en"] = "LAYERS";
 ln["mc.layers-list-controllet.checkedLayersButton.en"] = "Filter checked layers";
 ln["mc.layers-list-controllet.zoomLayerButton.en"] = "Zoom to selected layer";
+ln["mc.layers-list-controllet.search.en"] = "Search";
 
 ln["mc.map-preview-controllet.title.en"] = "MAP";
 ln["mc.map-preview-controllet.zoomLayersButton.en"] = "Zoom to all layers";
@@ -44,13 +44,230 @@ ln["mc.layer-layout-controllet.text-color-blue.en"] = "Font color blue";
 ln["mc.layer-layout-controllet.text-color-alpha.en"] = "Font color alpha";
 ln["mc.layer-layout-controllet.layer-styles.en"] = "Available styles for: ";
 
+ln["mc.map-preview-controllet.user-layers.en"] = "User layers";
+ln["mc.openlayers-map.base-map.en"] = "Base map";
+ln["mc.openlayers-map.osm-humanitarian-map.en"] = "OSM Humanitarian";
+ln["mc.openlayers-map.osm-landscape-map.en"] = "OSM Landscape";
+ln["mc.openlayers-map.osm-transport-map.en"] = "OSM Transporte";
+ln["mc.openlayers-map.osm-cycle-map.en"] = "OSM Cycle";
+ln["mc.openlayers-map.osm-standard-map.en"] = "OSM Standard";
+ln["mc.openlayers-map.zoom-in.en"] = "Zoom in";
+ln["mc.openlayers-map.zoom-out.en"] = "Zoom out";
 
 
+/*IT*/
+ln["mc.map-controllet.slide1_title.it"] = "SELEZIONA UN DATASET";
+ln["mc.map-controllet.slide1_subtitle.it"] = "Seleziona un dataset dall'elenco oppure copia-incolla l'url di un dataset.";
+ln["mc.map-controllet.slide2_title.it"] = "SELEZIONA I LIVELLI";
+ln["mc.map-controllet.slide2_subtitle.it"] = "Seleziona i livelli a sinistra. La mappa mostrerà i livelli selezionati.";
+
+ln["mc.map-controllet.addDatasetButton.it"] = "Aggiungi Dataset";
+ln["mc.map-controllet.addDataletButton.it"] = "Aggiungi";
+
+ln["mc.layers-list-controllet.title.it"] = "LIVELLI";
+ln["mc.layers-list-controllet.checkedLayersButton.it"] = "Filtra i livelli selezionati";
+ln["mc.layers-list-controllet.zoomLayerButton.it"] = "Zoom al livello selezionato";
+ln["mc.layers-list-controllet.user-layers.it"] = "User layers";
+
+ln["mc.map-preview-controllet.title.it"] = "MAPPA";
+ln["mc.map-preview-controllet.zoomLayersButton.it"] = "Zoom su tutti i livelli";
+ln["mc.map-preview-controllet.user-layers.it"] = "Livelli utente";
+
+ln["mc.layer-layout-controllet.title.it"] = "LAYOUT";
+ln["mc.layer-layout-controllet.stroke-style.it"] = "Stile spessore";
+ln["mc.layer-layout-controllet.stroke-width.it"] = "Spessore del tratto";
+ln["mc.layer-layout-controllet.stroke-color-red.it"] = "Spessore rosso";
+ln["mc.layer-layout-controllet.stroke-color-green.it"] = "Spessore verde";
+ln["mc.layer-layout-controllet.stroke-color-blue.it"] = "Spessore blu";
+ln["mc.layer-layout-controllet.stroke-color-alpha.it"] = "Spessore colore ad alfa";
+ln["mc.layer-layout-controllet.fill-style.it"] = "Stile riempimento";
+ln["mc.layer-layout-controllet.fill-color-red.it"] = "Riempimento rosso";
+ln["mc.layer-layout-controllet.fill-color-green.it"] = "Riempimento verde";
+ln["mc.layer-layout-controllet.fill-color-blue.it"] = "Riempimento blu";
+ln["mc.layer-layout-controllet.fill-color-alpha.it"] = "Riempimento colore ad alfa";
+ln["mc.layer-layout-controllet.point-style.it"] = "Stile riempimento";
+ln["mc.layer-layout-controllet.icon-color-red.it"] = " color red";
+ln["mc.layer-layout-controllet.icon-color-green.it"] = "Riempimento verde";
+ln["mc.layer-layout-controllet.icon-color-blue.it"] = " Riempimento blu";
+ln["mc.layer-layout-controllet.icon-color-alpha.it"] = "Riempimento colore ad  alfa";
+ln["mc.layer-layout-controllet.label-style.it"] = "Stile etichette";
+ln["mc.layer-layout-controllet.property-name.it"] = "Campo";
+ln["mc.layer-layout-controllet.font-name.it"] = "Tipo di carattere";
+ln["mc.layer-layout-controllet.font-size.it"] = "Dimensione carattere";
+ln["mc.layer-layout-controllet.text-color-red.it"] = "Carattere di colore rosso";
+ln["mc.layer-layout-controllet.text-color-green.it"] = "Carattere di colore verde";
+ln["mc.layer-layout-controllet.text-color-blue.it"] = "Carattere di colore blu";
+ln["mc.layer-layout-controllet.text-color-alpha.it"] = "Carattere di colore ad alfa";
+ln["mc.layer-layout-controllet.layer-styles.it"] = "Stili disponibile per: ";
+ln["mc.layers-list-controllet.search.it"] = "Cerca";
+ln["mc.openlayers-map.base-map.it"] = "Livelli mappa";
+ln["mc.openlayers-map.osm-humanitarian-map.it"] = "Umanitario";
+ln["mc.openlayers-map.osm-landscape-map.it"] = "Mappa del Paesaggio";
+ln["mc.openlayers-map.osm-transport-map.it"] = "Mappa dei Trasport";
+ln["mc.openlayers-map.osm-cycle-map.it"] = "Mappa Ciclabile";
+ln["mc.openlayers-map.osm-standard-map.it"] = "Mappa Standard";
+ln["mc.openlayers-map.zoom-in.it"] = "Aumenta Zoom";
+ln["mc.openlayers-map.zoom-out.it"] = "Diminuisci Zoom";
+
+/*FR*/
+ln["mc.map-controllet.slide1_title.fr"] = "SELECT DATASET";
+ln["mc.map-controllet.slide1_subtitle.fr"] = "Select a dataset from the list or copy and paste the url of dataset.";
+ln["mc.map-controllet.slide2_title.fr"] = "SELECT LAYERS";
+ln["mc.map-controllet.slide2_subtitle.fr"] = "Select the layers on the left. The map will show the selected layers.";
+
+ln["mc.map-controllet.addDatasetButton.fr"] = "Add Dataset";
+ln["mc.map-controllet.addDataletButton.fr"] = "Add";
+
+ln["mc.layers-list-controllet.title.fr"] = "LAYERS";
+ln["mc.layers-list-controllet.checkedLayersButton.fr"] = "Filter checked layers";
+ln["mc.layers-list-controllet.zoomLayerButton.fr"] = "Zoom to selected layer";
+ln["mc.layers-list-controllet.search.fr"] = "Search";
+
+ln["mc.map-preview-controllet.title.fr"] = "MAP";
+ln["mc.map-preview-controllet.zoomLayersButton.fr"] = "Zoom to all layers";
+
+ln["mc.layer-layout-controllet.title.fr"] = "LAYOUT";
+ln["mc.layer-layout-controllet.stroke-style.fr"] = "Stroke style";
+ln["mc.layer-layout-controllet.stroke-width.fr"] = "Stroke width";
+ln["mc.layer-layout-controllet.stroke-color-red.fr"] = "Stroke color red";
+ln["mc.layer-layout-controllet.stroke-color-green.fr"] = "Stroke color green";
+ln["mc.layer-layout-controllet.stroke-color-blue.fr"] = "Stroke color blue";
+ln["mc.layer-layout-controllet.stroke-color-alpha.fr"] = "Stroke color alpha";
+ln["mc.layer-layout-controllet.fill-style.fr"] = "Fill style";
+ln["mc.layer-layout-controllet.fill-color-red.fr"] = "Fill color red";
+ln["mc.layer-layout-controllet.fill-color-green.fr"] = "Fill color green";
+ln["mc.layer-layout-controllet.fill-color-blue.fr"] = "Fill color blue";
+ln["mc.layer-layout-controllet.fill-color-alpha.fr"] = "Fill color alpha";
+ln["mc.layer-layout-controllet.point-style.fr"] = "Fill style";
+ln["mc.layer-layout-controllet.icon-color-red.fr"] = "Fill color red";
+ln["mc.layer-layout-controllet.icon-color-green.fr"] = "Fill color green";
+ln["mc.layer-layout-controllet.icon-color-blue.fr"] = "Fill color blue";
+ln["mc.layer-layout-controllet.icon-color-alpha.fr"] = "Fill color alpha";
+ln["mc.layer-layout-controllet.label-style.fr"] = "Label style";
+ln["mc.layer-layout-controllet.property-name.fr"] = "Property name";
+ln["mc.layer-layout-controllet.font-name.fr"] = "Font name";
+ln["mc.layer-layout-controllet.font-size.fr"] = "Font size";
+ln["mc.layer-layout-controllet.text-color-red.fr"] = "Font color red";
+ln["mc.layer-layout-controllet.text-color-green.fr"] = "Font color green";
+ln["mc.layer-layout-controllet.text-color-blue.fr"] = "Font color blue";
+ln["mc.layer-layout-controllet.text-color-alpha.fr"] = "Font color alpha";
+ln["mc.layer-layout-controllet.layer-styles.fr"] = "Available styles for: ";
+
+ln["mc.map-preview-controllet.user-layers.fr"] = "User layers";
+ln["mc.openlayers-map.base-map.fr"] = "Base map";
+ln["mc.openlayers-map.osm-humanitarian-map.fr"] = "OSM Humanitarian";
+ln["mc.openlayers-map.osm-landscape-map.fr"] = "OSM Landscape";
+ln["mc.openlayers-map.osm-transport-map.fr"] = "OSM Transporte";
+ln["mc.openlayers-map.osm-cycle-map.fr"] = "OSM Cycle";
+ln["mc.openlayers-map.osm-standard-map.fr"] = "OSM Standard";
+ln["mc.openlayers-map.zoom-in.fr"] = "Zoom in";
+ln["mc.openlayers-map.zoom-out.fr"] = "Zoom out";
+
+/*NL*/
+ln["mc.map-controllet.slide1_title.nl"] = "SELECT DATASET";
+ln["mc.map-controllet.slide1_subtitle.nl"] = "Select a dataset from the list or copy and paste the url of dataset.";
+ln["mc.map-controllet.slide2_title.nl"] = "SELECT LAYERS";
+ln["mc.map-controllet.slide2_subtitle.nl"] = "Select the layers on the left. The map will show the selected layers.";
+
+ln["mc.map-controllet.addDatasetButton.nl"] = "Add Dataset";
+ln["mc.map-controllet.addDataletButton.nl"] = "Add";
+
+ln["mc.layers-list-controllet.title.nl"] = "LAYERS";
+ln["mc.layers-list-controllet.checkedLayersButton.nl"] = "Filter checked layers";
+ln["mc.layers-list-controllet.zoomLayerButton.nl"] = "Zoom to selected layer";
+ln["mc.layers-list-controllet.search.nl"] = "Search";
+
+ln["mc.map-preview-controllet.title.nl"] = "MAP";
+ln["mc.map-preview-controllet.zoomLayersButton.nl"] = "Zoom to all layers";
+
+ln["mc.layer-layout-controllet.title.nl"] = "LAYOUT";
+ln["mc.layer-layout-controllet.stroke-style.nl"] = "Stroke style";
+ln["mc.layer-layout-controllet.stroke-width.nl"] = "Stroke width";
+ln["mc.layer-layout-controllet.stroke-color-red.nl"] = "Stroke color red";
+ln["mc.layer-layout-controllet.stroke-color-green.nl"] = "Stroke color green";
+ln["mc.layer-layout-controllet.stroke-color-blue.nl"] = "Stroke color blue";
+ln["mc.layer-layout-controllet.stroke-color-alpha.nl"] = "Stroke color al-pha";
+ln["mc.layer-layout-controllet.fill-style.nl"] = "Fill style";
+ln["mc.layer-layout-controllet.fill-color-red.nl"] = "Fill color red";
+ln["mc.layer-layout-controllet.fill-color-green.nl"] = "Fill color green";
+ln["mc.layer-layout-controllet.fill-color-blue.nl"] = "Fill color blue";
+ln["mc.layer-layout-controllet.fill-color-alpha.nl"] = "Fill color alpha";
+ln["mc.layer-layout-controllet.point-style.nl"] = "Fill style";
+ln["mc.layer-layout-controllet.icon-color-red.nl"] = "Fill color red";
+ln["mc.layer-layout-controllet.icon-color-green.nl"] = "Fill color green";
+ln["mc.layer-layout-controllet.icon-color-blue.nl"] = "Fill color blue";
+ln["mc.layer-layout-controllet.icon-color-alpha.nl"] = "Fill color alpha";
+ln["mc.layer-layout-controllet.label-style.nl"] = "Label style";
+ln["mc.layer-layout-controllet.property-name.nl"] = "Property name";
+ln["mc.layer-layout-controllet.font-name.nl"] = "Font name";
+ln["mc.layer-layout-controllet.font-size.nl"] = "Font size";
+ln["mc.layer-layout-controllet.text-color-red.nl"] = "Font color red";
+ln["mc.layer-layout-controllet.text-color-green.nl"] = "Font color green";
+ln["mc.layer-layout-controllet.text-color-blue.nl"] = "Font color blue";
+ln["mc.layer-layout-controllet.text-color-alpha.nl"] = "Font color alpha";
+ln["mc.layer-layout-controllet.layer-styles.nl"] = "Available styles for: ";
+
+ln["mc.map-preview-controllet.user-layers.nl"] = "User layers";
+ln["mc.openlayers-map.base-map.nl"] = "Base map";
+ln["mc.openlayers-map.osm-humanitarian-map.nl"] = "OSM Humanitarian";
+ln["mc.openlayers-map.osm-landscape-map.nl"] = "OSM Landscape";
+ln["mc.openlayers-map.osm-transport-map.nl"] = "OSM Transporte";
+ln["mc.openlayers-map.osm-cycle-map.nl"] = "OSM Cycle";
+ln["mc.openlayers-map.osm-standard-map.nl"] = "OSM Standard";
+ln["mc.openlayers-map.zoom-in.nl"] = "Zoom in";
+ln["mc.openlayers-map.zoom-out.nl"] = "Zoom out";
 
 
+/*es-ES*/
+ln["mc.map-controllet.slide1_title.es-ES"] = "SELECT DATASET";
+ln["mc.map-controllet.slide1_subtitle.es-ES"] = "Select a dataset from the list or copy and paste the url of dataset.";
+ln["mc.map-controllet.slide2_title.es-ES"] = "SELECT LAYERS";
+ln["mc.map-controllet.slide2_subtitle.es-ES"] = "Select the layers on the left. The map will show the selected layers.";
 
+ln["mc.map-controllet.addDatasetButton.es-ES"] = "Add Dataset";
+ln["mc.map-controllet.addDataletButton.es-ES"] = "Add";
 
+ln["mc.layers-list-controllet.title.es-ES"] = "LAYERS";
+ln["mc.layers-list-controllet.checkedLayersButton.es-ES"] = "Filter checked layers";
+ln["mc.layers-list-controllet.zoomLayerButton.es-ES"] = "Zoom to selected layer";
+ln["mc.layers-list-controllet.search.es-ES"] = "Search";
 
+ln["mc.map-preview-controllet.title.es-ES"] = "MAP";
+ln["mc.map-preview-controllet.zoomLayersButton.es-ES"] = "Zoom to all layers";
 
+ln["mc.layer-layout-controllet.title.es-ES"] = "LAYOUT";
+ln["mc.layer-layout-controllet.stroke-style.es-ES"] = "Stroke style";
+ln["mc.layer-layout-controllet.stroke-width.es-ES"] = "Stroke width";
+ln["mc.layer-layout-controllet.stroke-color-red.es-ES"] = "Stroke color red";
+ln["mc.layer-layout-controllet.stroke-color-green.es-ES"] = "Stroke color green";
+ln["mc.layer-layout-controllet.stroke-color-blue.es-ES"] = "Stroke color blue";
+ln["mc.layer-layout-controllet.stroke-color-alpha.es-ES"] = "Stroke color al-pha";
+ln["mc.layer-layout-controllet.fill-style.es-ES"] = "Fill style";
+ln["mc.layer-layout-controllet.fill-color-red.es-ES"] = "Fill color red";
+ln["mc.layer-layout-controllet.fill-color-green.es-ES"] = "Fill color green";
+ln["mc.layer-layout-controllet.fill-color-blue.es-ES"] = "Fill color blue";
+ln["mc.layer-layout-controllet.fill-color-alpha.es-ES"] = "Fill color alpha";
+ln["mc.layer-layout-controllet.point-style.es-ES"] = "Fill style";
+ln["mc.layer-layout-controllet.icon-color-red.es-ES"] = "Fill color red";
+ln["mc.layer-layout-controllet.icon-color-green.es-ES"] = "Fill color green";
+ln["mc.layer-layout-controllet.icon-color-blue.es-ES"] = "Fill color blue";
+ln["mc.layer-layout-controllet.icon-color-alpha.es-ES"] = "Fill color alpha";
+ln["mc.layer-layout-controllet.label-style.es-ES"] = "Label style";
+ln["mc.layer-layout-controllet.property-name.es-ES"] = "Property name";
+ln["mc.layer-layout-controllet.font-name.es-ES"] = "Font name";
+ln["mc.layer-layout-controllet.font-size.es-ES"] = "Font size";
+ln["mc.layer-layout-controllet.text-color-red.es-ES"] = "Font color red";
+ln["mc.layer-layout-controllet.text-color-green.es-ES"] = "Font color green";
+ln["mc.layer-layout-controllet.text-color-blue.es-ES"] = "Font color blue";
+ln["mc.layer-layout-controllet.text-color-alpha.es-ES"] = "Font color alpha";
+ln["mc.layer-layout-controllet.layer-styles.es-ES"] = "Available styles for: ";
 
-
+ln["mc.openlayers-map.zoom-in.es-ES"] = "Zoom in";
+ln["mc.openlayers-map.zoom-in.es-ES"] = "Zoom out";
+ln["mc.map-preview-controllet.user-layers.es-ES"] = "User layers";
+ln["mc.openlayers-map.base-map.es-ES"] = "Base map";
+ln["mc.openlayers-map.osm-humanitarian-map.es-ES"] = "OSM Humanitarian";
+ln["mc.openlayers-map.osm-landscape-map.es-ES"] = "OSM Landscape";
+ln["mc.openlayers-map.osm-transport-map.es-ES"] = "OSM Transporte";
+ln["mc.openlayers-map.osm-cycle-map.es-ES"] = "OSM Cycle";
+ln["mc.openlayers-map.osm-standard-map.es-ES"] = "OSM Standard";
